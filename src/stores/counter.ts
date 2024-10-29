bf841a9fbaa06ae8637ec8eefd31310acae25156
daVinci <daVinci-10@outlook.com>
2024-10-29 14:31:23 +0800

    test

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,11 +1,9 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore = defineStore('counter', {
+  state: () => {
+
   }
 
   return { count, doubleCount, increment }
